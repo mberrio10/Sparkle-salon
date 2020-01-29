8ba5fef6c2d8b4169a6e0c51140ad90fb1063496
Mike Berrio <mikeberrio10@hotmail.com>
2020-01-29 16:48:38 +0000

    pics added

--- src/js/component/Components/CarouselHair.js
+++ src/js/component/Components/CarouselHair.js
@@ -34,7 +34,7 @@ export class CarouselHair extends React.Component {
 							</div>
 							<div className="carousel-item">
 								<img
-									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20191128-WA0003.jpg?alt=media&token=2cf6a164-5566-4245-9a9d-f8be11994fc7"
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200127-WA0003.jpg?alt=media&token=7eae2df7-92a7-4852-ac01-7a4520e534a5"
 									className="d-block w-100"
 									alt="..."
 								/>
@@ -42,6 +42,83 @@ export class CarouselHair extends React.Component {
 							<div className="carousel-item">
 								<img
 									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20191128-WA0007.jpg?alt=media&token=3f760b6c-05a5-4e26-9ede-011492585ca5"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200115-WA0000.jpg?alt=media&token=b365c1d9-e5fc-4669-ba02-2ee6a3ccbeae"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20191128-WA0006.jpg?alt=media&token=4e87a598-68a5-40bf-9344-e68181f028fa"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20191128-WA0002.jpg?alt=media&token=c411d407-e745-448b-9a23-bf98187359a1"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200115-WA0001.jpg?alt=media&token=a6e308d9-e603-4c15-90c0-8e54a43c1ec0"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200127-WA0000.jpg?alt=media&token=f0ff2d1f-2133-4db9-b6b3-c4199fe14c57"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200127-WA0002.jpg?alt=media&token=b4bb54d5-0503-49db-8a40-da0c60406e58"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200115-WA0003.jpg?alt=media&token=6a609ca5-fe88-466d-a2b8-e7a6549dd8fe"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200115-WA0004.jpg?alt=media&token=bc191cef-0253-49b1-a10c-a99fd0ae117f"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200127-WA0004.jpg?alt=media&token=c7aa3c8f-887c-47dd-ba1c-c6c55138d335"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200115-WA0006.jpg?alt=media&token=99c8cf9c-7214-4b91-8f5f-9549c1f4baba"
+									className="d-block w-100"
+									alt="..."
+								/>
+							</div>
+							<div className="carousel-item">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/salonphotos-ce4a2.appspot.com/o/IMG-20200127-WA0005.jpg?alt=media&token=72d7c14d-f8af-4c7f-8a76-6e1725381529"
 									className="d-block w-100"
 									alt="..."
 								/>
