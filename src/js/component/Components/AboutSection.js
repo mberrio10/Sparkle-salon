f888f11b7b0bcfbfe04dd44f5fcbf97528d2d5d3
Mike Berrio <mikeberrio10@hotmail.com>
2021-03-04 22:37:43 +0000

    sparkle salon redesign

--- src/js/component/Components/AboutSection.js
+++ src/js/component/Components/AboutSection.js
@@ -8,8 +8,9 @@ export class AboutSection extends React.Component {
 					<h2 className="heading-secondary">Our Salon</h2>
 				</div>
 				<div className="row">
+					<div className="col-lg-6 col-md-6 col-sm-12">images</div>
 					<div className="col-lg-6 col-md-6 col-sm-12">
-						<h5 className="description lead">
+						<p className="paragraph">
 							Located in Coral Springs, Sparkle Beauty Salon is
 							committed to providing total beauty and satisfaction
 							while creating a unique portrait of style for each
@@ -18,7 +19,10 @@ export class AboutSection extends React.Component {
 							an exquisite range of innovative and elgant styles.
 							Your hair is your best accessory: designed by us,
 							worn beautifully by you!
-						</h5>
+						</p>
+						<a href="#" className="btn-text">
+							Contact Us &rarr;
+						</a>
 					</div>
 				</div>
 			</div>
